8c83abd9cc323205e7db091b11696eec3326fdc1
Élise Philippe <elise.ph@icloud.com>
2020-06-23 22:02:23 +0200

    did some design on the navbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import NavBar from "./views/NavBar";
+import Old from "./views/Old";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <div className="NotNavBar">
+      <h1>Dashboard</h1>
+    </div>
+
+    {/* <Old /> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/views/NavBar/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./NavBar.css";
+import logoCalendar from "../../assets/calendar.svg";
+import logoEuro from "../../assets/euro.svg";
+import logoThinking from "../../assets/thinking.svg";
+import logoPorteEponge from "../../assets/porte-eponge.jpg";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <div className="NavItem">
+        <img src={logoPorteEponge} alt="porte éponge"></img>
+      </div>
+      <div className="NavItem">
+        <img src={logoCalendar} alt="calendar icon"></img>
+      </div>
+      <div className="NavItem">
+        <img src={logoEuro} alt="euro icon"></img>
+      </div>
+      <div className="NavItem">
+        <img src={logoThinking} alt="thinking bubble icon"></img>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
